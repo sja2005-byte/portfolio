334ba6b58cf02cbb41f37a31aa4bec3648ad1a4c
sja2005-byte <gjaegyu5@gmail.com>
2025-10-10 23:19:05 +0900

    Add files via upload

---
+++ js/find_id2.js
@@ -0,0 +1,7 @@
+// let study = prompt("공부할 내용을 입력하시오","이름을 입력하시오");
+//         console.log(find-container);
+function submitUsername() {
+    const username = document.getElementById('email').value;
+    
+    alert('아이디를 찾았습니다.');
+}
